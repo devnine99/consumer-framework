dfb5606d36d18986006f93500f757ab71f84178a
devnine99 <devnine99@gmail.com>
2021-11-05 14:13:47 +0900

    initial commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='consumer-framework',
-    version='0.0.2',
+    version='0.0.3',
     description='Consumer Framework',
     author='devnine99',
     author_email='devnine99@gmail.com',

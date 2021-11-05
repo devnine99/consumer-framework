5149a34f2a6b5920f15f842a0d8a000175c0d113
devnine99 <devnine99@gmail.com>
2021-11-06 00:50:12 +0900

    initial commit

--- consumer_framework/__main__.py
+++ consumer_framework/__main__.py
@@ -1,24 +1,23 @@
+import logging
+import os
 import sys
 import importlib.util
 
 from consumer_framework.app import ConsumerFramework
+from consumer_framework.command_parser import CommandParser
+
+logger = logging.getLogger(__name__)
 
 
 def main():
-    args = sys.argv[1:]
-    try:
-        app_index = args.index('-A')
-        app = args[app_index+1]
-    except (ValueError, IndexError):
-        print('앱이 지정되지 않았습니다.')
-        return
+    parser = CommandParser(sys.argv[1:])
 
-    module_name = 'consumer_framework'
-    module_path = '/'.join(sys.argv[0].split('/')[:-1]) + f'/{app.replace(".", "/")}.py'
+    module_path = f'{os.getcwd()}/{parser.app.replace(".", "/")}.py'
 
-    spec = importlib.util.spec_from_file_location(module_name, module_path)
+    spec = importlib.util.spec_from_file_location('consumer_framework', module_path)
     mod = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(mod)
+
     for m in mod.__dir__():
         if isinstance(getattr(mod, m), ConsumerFramework):
             sys.exit(getattr(mod, m).run())
